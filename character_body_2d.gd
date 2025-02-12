92d8e2026aa559232a9d5bf5ae815da25785ad28
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-02-12 22:06:13 +0100

    gaurd chasing

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,24 +1,24 @@
 extends CharacterBody2D
-
+var SPEED = 50
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	player = get_node("../../player/player01")
-	var direction = (player.position - self.position).normalized()
-	if direction.x > 0:
-		if chase == true:
-			print ("Chase Right")
-	else:
-		print("Left")
-
+	if chase == true:
+		player = get_node("../../player/player01")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = false
+		else:
+			get_node("AnimatedSprite2D").flip_h = true
+		velocity.x = direction.x * SPEED
 	move_and_slide()
 func _on_player_detection_body_entered(body):
 	if body.name == "player01":
 		chase = true
 
 
-func _on_player_detection_body_exited(body: Node2D) -> void:
+func _on_player_detection_body_exited(body):
 	if body.name == "player01":
 		chase = false
