add14b1940809b8c04b0a71fae22a67e7e710c27
Mate9000 <1213286@student.roc-nijmegen.nl>
2025-02-12 21:19:46 +0100

    extra add ons

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,16 +2,23 @@ extends CharacterBody2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
+var chase = false
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	
+	player = get_node("../../player/player01")
+	var direction = (player.position - self.position).normalized()
+	if direction.x > 0:
+		if chase == true:
+			print ("Chase Right")
+	else:
+		print("Left")
+
 	move_and_slide()
 func _on_player_detection_body_entered(body):
 	if body.name == "player01":
-		player = get_node("../../player/player01")
-		var direction = (player.position - self.position).normalized()
-		if direction.x > 0:
-			
-			print("Right")
-		else:
-			print("Left")
+		chase = true
+
+
+func _on_player_detection_body_exited(body: Node2D) -> void:
+	if body.name == "player01":
+		chase = false
